a9181e4d196660712f921146de5b903ce969a0c5
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2021-10-15 16:46:55 +0100

    2D List example

---
+++ datastructures/two_dim_arr.py
@@ -0,0 +1,9 @@
+class_data = [[23,43,56],
+              [34,44,23],
+              [34,44,55]]
+total = 0
+for row in class_data:
+  total = total + row[1]
+#next row
+
+print(total)
---
+++ examples/is_example.py
@@ -0,0 +1,7 @@
+a = 0
+b = 0
+while a is b:
+  print(a)
+  a = a - 1
+  b = b - 1
+#end while
