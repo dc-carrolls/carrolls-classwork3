4d33220e6e2ea7db60c3528c0af4535e38b9d05f
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2021-11-17 17:23:45 +0000

    Update parameters.py

--- subroutines/parameters.py
+++ subroutines/parameters.py
@@ -1,10 +1,32 @@
 def proc1 (x,y):
+	x = x - 2
+	y = 0
+#end procedure
+
+def proc2 (x,y):
+	x = x - 2
+	y[0] = 0
+#end procedure
+
+
+def proc3 (x,y):
 	x = (x[0] - 2,)
 	y[0] = 0
 #end procedure
 
 #main program
+
+m = 8
+n = 10
+proc1(m, n)
+print (m, n)
+
+m = 8
+n = [10]
+proc2(m, n)
+print (m, n)
+
 m = (8,) 
 n = [10]
-proc1(m, n)
+proc3(m, n)
 print (m[0], n[0])
