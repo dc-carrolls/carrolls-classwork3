657a9d58fa3b2f7dae0a6ccd6910a749dd81cd2d
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2021-11-18 11:20:51 +0000

    oop

---
+++ subroutines/carpark.py
@@ -0,0 +1,45 @@
+car_park =[]
+for row in range(20):
+  car_park.append([])
+  for col in range(6):
+    car_park[row].append('empty')
+  #next col
+#next row
+
+
+car_park[3][4] = 'BHS 12RZ'
+
+print(car_park)
+
+
+# 	// main program
+# 	initialise car park grid to “empty”
+# #display menu of options
+# print("1. Reset all spaces in the car park to 'empty'")
+# print("2. Park a car")
+# print("3. Remove a car")
+# print("4. Display the car park grid")
+# print("5. Quit\n")
+# option = input("Enter your choice: ")
+# // accept choice
+# while option != "5"
+#     if option == "1"
+#         emptyCarPark(carPark)
+#     else if option == "2"
+#         parkACar(carPark)
+#     else if option == "3"
+#         removeACar(carPark)
+#     else if option == "4"
+#         displayCarParkGrid(carPark)
+#     else
+#         option = ("Invalid choice - please re-enter: ")
+# 	endif
+#     print("1. Reset all spaces in the car park to ‘empty’")
+#     print("2. Park a car")
+#     print("3. Remove a car")
+#     print("4. Display the car park grid")
+#     print("5. Quit\n")
+#     option = input("Enter your choice: ")
+# endwhile
+# print("Goodbye") 
+
