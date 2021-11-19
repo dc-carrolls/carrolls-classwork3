a3671f5e8835b0772121568899b20e207353c2f1
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2021-11-19 11:43:17 +0000

    Update oneman.py

--- datastructures/dictionary/oneman.py
+++ datastructures/dictionary/oneman.py
@@ -1,5 +1,13 @@
 mow = {'one':6,'man':6,'went':16,'to':17,'mow':17,'a':12,'meadow':12,
        'and':5,'his':5,'dog':5,'two':5,'three':4,'four':3,'five':2,'men':14}
 
+
+print(mow['one'])
+mow['cat']=0
+print(mow)
+print(mow.pop('cat'))
+
 if 'cat' in mow:
-  print('dog')
+  print('Yes')
+else:
+  print('No')
