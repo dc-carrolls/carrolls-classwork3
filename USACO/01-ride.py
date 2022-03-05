5f5528a06194c1acb7dd9f0c206e4a26302ef0df
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2022-03-05 06:23:54 +0000

    USACO ride 1

--- USACO/01-ride.py
+++ USACO/01-ride.py
@@ -3,19 +3,34 @@ ID: src.dul1
 LANG: PYTHON3
 TASK: ride
 """
-fin = open ('./USACO/test.in', 'r')
-fout = open ('./USACO/test.out', 'w')
 
-comet = fin.readline()
-group = fin.readline()
+import pathlib
+
+#print(pathlib.Path(__file__).parent.resolve()) 
+
+fin = open (str(pathlib.Path(__file__).parent.resolve())+'/test.in', 'r')
+fout = open (str(pathlib.Path(__file__).parent.resolve())+'/test.out', 'w')
+
+comet = fin.readline().strip()
+group = fin.readline().strip()
 
 comet_no = 1
 for c in comet:
   comet_no *= (ord(c)-64)
 comet_no %= 47
 
+group_no = 1
+for c in group:
+  group_no *= (ord(c)-64)
+group_no %= 47
+
 
 # x,y = map(int, fin.readline().split())
 # sum = x+y
-fout.write (str(comet_no) + '\n')
+
+if group_no == comet_no:
+  fout.write ('GO\n')
+else:
+  fout.write ('STAY\n')
 fout.close()
+fin.close()
