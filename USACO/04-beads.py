739d491904fdbf4ff13c73588ac21fe3d0463407
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2022-03-07 10:46:17 +0000

    Update 04-beads.py

--- USACO/04-beads.py
+++ USACO/04-beads.py
@@ -13,32 +13,35 @@ fout = open (pathlib.Path(__file__).parent.resolve() / 'beads.out', 'w')
 n = int(fin.readline().strip())
 nl = fin.readline().strip()
 
-lmax=1
-l = 1
 
 cc=nl[0]
-change = 0
-i=1
-while i < 2*n:
-  c = nl[i%n]
-  if change < 2:
-    if (c == cc) or (c=='w'): 
-      l+=1
-      if l > lmax:
-        lmax = l
-      #end if
-    else:
-      change+=1
-      cc=c
+if cc == 'w':
+  cc='b'
+sp=0
+np=0
+ep=1
+l = ep-sp
+lmax=l
+while sp < n and (ep-sp) < n:
+  c = nl[ep%n] 
+  if c=='w':
+    c=cc 
+  if (c==cc) or (c=='w'): 
+    l=ep-sp
+    if l > lmax:
+      lmax = l
+    #  print('max:',lmax,'sp:',sp,'ep:',ep)
     #end if
   else:
-    l=1
-    change=0
+    cc=c
+    sp=np
+    np=ep
   #end if
-  i+=1
+  ep+=1
 #end while
 print(n)
 print(nl)
+print('start:',sp,'end:',ep)
 print(lmax)
 
 fout.write ('STAY\n')
