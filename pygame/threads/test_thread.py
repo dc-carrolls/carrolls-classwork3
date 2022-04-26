349ee73a07ce142b8a8945c631ee9d3f524b40f1
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2022-04-26 17:47:16 +0100

    Create test_thread.py

---
+++ pygame/threads/test_thread.py
@@ -0,0 +1,85 @@
+from threading import Thread
+import pygame
+from time import sleep
+from random import randint
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+running = [False]
+x_pos = 0
+
+def task(id):
+  print(x:=randint(1,10))
+  sleep(x)
+  
+  # x = 0
+  # while x < 100000000:
+  #   x += 1
+  # print('finished',x)
+  id[0] =False
+
+
+
+
+pygame.init()
+ 
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("My Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+    # --- Game logic should go here
+      
+
+
+    if not running[0]:
+      running[0] = True
+      t = Thread(target=task, args=(running,))
+      t.start()
+      x_pos = 0
+    
+      
+    x_pos += 1
+
+
+ 
+    # --- Screen-clearing code goes here
+ 
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+ 
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(WHITE)
+ 
+    # --- Drawing code should go here
+    pygame.draw.rect(screen, RED, [x_pos,100,10,10])  
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+pygame.quit()
+
+
